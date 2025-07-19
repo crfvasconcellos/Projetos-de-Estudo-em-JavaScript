14e08eb7bccf6169ea57160fbf277846c5d80191
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-07-18 23:27:32 -0300

    Create ex011.js

---
+++ Curso em Vídeo/Aula 12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 17
+
+if (idade<16){
+    console.log('Não Pode Votar')
+}
+else if(idade>=16 && idade<18){
+    console.log ('Voto não obrigatório mas pode')
+}
+else{
+    console.log('Voto Obrigatório')
+}
